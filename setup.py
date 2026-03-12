41b4ab7b86e91b61d755e051561f97e5a26b976b
Hongyu Tseng <>
2026-03-12 15:12:18 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements():
 
 setup(
     name='sygna-bridge-util',
-    version='2.0.3',
+    version='2.0.4',
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=get_requirements(),
